41a7ec073fb873bb59d74a00455ba4186492e71a
JaxzTan <129146389+JaxzTan@users.noreply.github.com>
2026-09-14 14:10:41 +0800

    feat: delete all source code tmo

--- packages/board-codec/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./types";
-export { numberSteps, buildNumberMap } from "./numbering";
-export { parseMarkdown } from "./parse";
-export { serializeToMarkdown } from "./serialize";
--- prisma/migrations/20260813010000_visible_fields_drop_quadrant/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Board" ADD COLUMN     "visibleFields" TEXT[] DEFAULT ARRAY['due', 'prio', 'blocker']::TEXT[];
-
--- AlterTable
-ALTER TABLE "Node" DROP COLUMN "quadrant";
-
--- DropEnum
-DROP TYPE "Quadrant";
